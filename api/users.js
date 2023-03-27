76fe606234d170b4e8093a81d9bf92fffbb478fa
MangeshaJadhav <mangeshjadhav987@gmail.com>
2023-03-27 20:45:03 +0530

    Assignment done

---
+++ api/users.js
@@ -0,0 +1,37 @@
+export const Users = [
+  {
+    id: 1,
+    first_name: "ram",
+    last_name: "patil",
+    email: "ram@gmail.com",
+    gender: "male",
+  },
+  {
+    id: 2,
+    first_name: "sham",
+    last_name: "patil",
+    email: "sham@gmail.com",
+    gender: "male",
+  },
+  {
+    id: 3,
+    first_name: "Mangesh",
+    last_name: "Jadhav",
+    email: "mangesh2@gmail.org",
+    gender: "male",
+  },
+  {
+    id: 4,
+    first_name: "Ganesh",
+    last_name: "sonawane",
+    email: "ganesh@gmail.com",
+    gender: "male",
+  },
+  {
+    id: 5,
+    first_name: "Priyanka",
+    last_name: "kale",
+    email: "pri@gmail.com",
+    gender: "Female",
+  },
+];
